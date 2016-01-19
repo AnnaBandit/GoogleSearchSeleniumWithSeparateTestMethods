20914b563161bb65e3474db3ad972d1abef76997
anna_goncharova <anna@bandit-ltd.com>
2016-01-19 10:19:59 +0200

    small update

--- src/test/java/ua/com/anya/Pages/GoogleSearchPage.java
+++ src/test/java/ua/com/anya/Pages/GoogleSearchPage.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
@@ -30,8 +29,6 @@ public class GoogleSearchPage {
 
     public SeleniumPage openFirstLink(){
         searchResults.get(0).findElement(By.cssSelector(".r>a")).click();
-        return PageFactory.initElements(driver, SeleniumPage.class);
+        return new SeleniumPage();
     }
-
-
 }
--- src/test/java/ua/com/anya/Pages/SeleniumPage.java
+++ src/test/java/ua/com/anya/Pages/SeleniumPage.java
@@ -1,16 +1,10 @@
 package ua.com.anya.Pages;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
 public class SeleniumPage {
-
-    @FindBy(css = "#header h1 a")
-    public WebElement headerLinkOnSeleniumPage;
 
     public String headerText(WebDriver driver){
         return driver.getTitle();
     }
-
 }
