a652ef7bb02080894d54174cc31156d42a9173dd
anna_goncharova <anna@bandit-ltd.com>
2016-01-27 19:22:00 +0200

    small changes

--- src/main/java/ua/com/anya/core/Helpers.java
+++ src/main/java/ua/com/anya/core/Helpers.java
@@ -15,6 +15,10 @@ public class Helpers {
         new WebDriverWait(driver, Configuration.timeout).until(condition);
     }
 
+    public static void wait(ExpectedCondition condition, WebDriver driver, int timeout){
+        new WebDriverWait(driver, timeout).until(condition);
+    }
+
     public static WebElement get(List<WebElement> elements, int index, WebDriver driver) {
         return get(elements, index, driver, Configuration.timeout);
     }
